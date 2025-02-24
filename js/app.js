17e2bafcd8246ddff609229b23b1432ae93fbfb8
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-02-24 17:21:12 +0100

    feat: aggiunta sezione aquisizioni dati

--- js/app.js
+++ js/app.js
@@ -1,2 +1,10 @@
-const age= prompt ()
-const numberKilo = prompt ()
+// acquisizione dei dati
+const age= prompt ('Inserisci la tua età');
+const numberKilo = prompt ('Inserisci il kilometraggio');
+console.log(age)
+console.log(numberKilo)
+
+
+
+// calcolo prezzo in base al km
+
